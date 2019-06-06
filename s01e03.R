06c521ceccedf5608e1da82a47e4a04b49f68a95
Carlos López <carlos.lopez@massivetrend.com>
2019-06-06 02:07:41 -0500

    Added some starting code to complete episode 3

--- s01e03.R
+++ s01e03.R
@@ -1,8 +1,8 @@
 # # Temas a tratar:
 # 1. Lectura de datos desde un archivo Excel
-# 
-# 
-# 
+# 2. Uso de %in%
+# 3. Cambio de etiquetas en eje X verticalmente
+# 4. Graficación con ggplot2 con etiquetas de eje X verticalmente
 # 
 # 
 setwd("/Users/natorro/Desktop/r-bits")
@@ -13,19 +13,54 @@ my_data <- read.table(pipe("pbpaste"), sep = "\t", header = TRUE)
 #my_data <- read.table(file = "clipboard", 
 #                      sep = "\t", header=TRUE)
 
-
+filesheets <- excel_sheets("composicion_y_estructura_de_la_poblacion.xlsx")
+filesheets
 my_data2 <- read_excel("composicion_y_estructura_de_la_poblacion.xlsx", 
-                       sheet = "Cuadro 1.2")
-my_data2 <- 
-estados <- my_data2[15:46, 1]
-poblaciontotal <- my_data2[15:46, 4]
-pobhombres <- my_data2[15:46, 6]
-pobmujeres <- my_data2[15:46, 7]
-library(tibble)
+                       col_names = c("estados", "v2", "v3", "poblaciontotal", 
+                                     "v5", "pobhombres", "pobmujeres"), 
+                       range = paste(filesheets[3],"!", "A16:G47", sep = ""))
+population_data <- select(my_data2, estados, poblaciontotal, pobhombres, pobmujeres)
+
+# Let's read the data:
+victimas <- read.csv("victimas.csv")
+
+victimas_filtrado <- subset(victimas, 
+                            Subtipo.de.delito == "Homicidio doloso" | 
+                              Subtipo.de.delito == "Feminicidio")
+
+victimas_melt <- melt(victimas_filtrado, 
+                      c("Año", "Entidad"), 
+                      c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre",
+                        "Diciembre"))
+
+head(victimas_melt)
+
+na_index <- is.na(victimas_melt$Valor)
+victimas_melt <- victimas_melt[!na_index,]
+
+victimas_agregado <- aggregate(victimas_melt$Valor, 
+                               by = list(entidad = victimas_melt$Entidad), 
+                               sum)
+
+estados <- population_data$estados
+delagregado <- victimas_agregado$entidad
+estados %in% delagregado
+estados == delagregado
+
 library(dplyr)
-population_data <- tibble(estados = estados, poblaciontotal = poblaciontotal,
-                              pobhombres = pobhombres, pobmujeres =  pobmujeres)
-rename_all(population_data,c("estados", "poblaciontotal", "pobhombres", "pobmujeres"))
-View(population_data)
-names(population_data) <- 
+dataration <- inner_join(victimas_agregado, population_data)
+dataration
+
+dataration$porcentaje <- (dataration$x / dataration$poblaciontotal ) * 100
+head(dataration)
+dataration$estados <- as.factor(dataration$estados)
+plot(dataration$estados, dataration$porcentaje)
+plot(dataration$estados, dataration$porcentaje, mar=c(5, 4, 0, 2) + 0.1, xaxt="n")
+axis(1, at=1:32, labels=categories, las = 2, cex.axis = 0.8, main = "", xlab ="")
+library(ggplot2)
+ggplot(dataration, aes(x = estados, y = porcentaje)) +
+  theme_bw() + 
+  geom_bar(stat = "identity") + 
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
 
